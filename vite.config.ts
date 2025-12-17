fc33c96502182f1b3143d1671e2e41ed76dc4609
nekotyan2d <dmitryrychazhkov2019@gmail.com>
2025-12-18 01:34:33 +0300

    fix: заменил base url

--- vite.config.ts
+++ vite.config.ts
@@ -10,5 +10,5 @@ export default defineConfig({
             "@": fileURLToPath(new URL("./src", import.meta.url)),
         },
     },
-    base: process.env.NODE_ENV === "production" ? "/mirea-fibr-colors/" : "/",
+    base: "/mirea-fibr-colors/",
 });
